6c76c1e3e43cdd074d1353ead94a3b46cbf8a8c4
jwmatson <jwmatson@ualberta.ca>
2022-05-12 23:09:23 -0600

    trying to make it look less ugly

--- BtoBSite/store/static/scripts/part-details.js
+++ BtoBSite/store/static/scripts/part-details.js
@@ -1,14 +1,12 @@
 function displayAvailability(){
-    console.log("HI")
     isAvailable = document.getElementsByClassName('isAvailableImg')
     for(i = 0; i<isAvailable.length; i+=1){
-        console.log(isAvailable[i]);
         if(isAvailable[i].alt == "True"){ //If available, display check mark
             console.log("HI")
-            isAvailable[i].src="/static/images/redX.png"
+            isAvailable[i].src="/static/images/greenCheck.png"
         }//if
         else{
-            isAvailable[i].src="/static/images/greenCheck.png"
+            isAvailable[i].src="/static/images/redX.png"
 
         }//else
     }//for
