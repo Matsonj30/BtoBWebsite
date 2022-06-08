82ca12e9f079c9b1ab0b28c5ae148c1a674b7d76
jwmatson <jwmatson@ualberta.ca>
2022-06-07 21:00:57 -0600

    search bar started

--- BtoBSite/store/urls.py
+++ BtoBSite/store/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name="home-page"), #slash in front is unnessessary
+    path('parts/search', views.searchedValue, name="searchedPage"), #struggling because I wanted to have the same url more or less for two URLS
+    #Has to go first as parts/search would always go to the other view otherwise
     path('parts/<str:category_chosen>', views.filteredParts, name="filteredPage"), #order really matters here
+    #^ this works because our JS is passing a category_chosen parameter to this path
+    # If we just had an html url reference to this path, it would create an erorr noreverse match
     path('parts', views.parts, name="partsPage"),
-   
 ]
--- BtoBSite/store/views.py
+++ BtoBSite/store/views.py
@@ -26,6 +26,7 @@ def parts(request):
     })
 
 def filteredParts(request, category_chosen):
+    print("filteredpage")
     categories = Category.objects.all()
     brands = Brand.objects.all()
     
@@ -48,8 +49,14 @@ def filteredParts(request, category_chosen):
             })
    
 
-    
-        
+def searchedValue(request):
+    print("HERE")
+    categories = Category.objects.all()
+    brands = Brand.objects.all()
+    return render(request, 'partsPage.html',{
+        'categories':categories,
+        'brands':brands,
+    })
 
     #request currently does not know what to do if use same function to go thru multiple
     # queries, as some values might not be found 
